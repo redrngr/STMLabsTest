4370c237e354d6dbc73e16f3924502fdee036ee0
regrngr <davaylava@ya.ru>
2021-07-11 17:09:44 +0300

    some fix

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ class User {
     this.registered = user.registered.date;
   }
 }
-
+//Хранилище состояний для целого одного массива
 const state = { users: [] };
 
+//После отрисовки ДОМа вывожу индикатор загрузки, делаю запрос к апи.
+//В случае успеха, удаляю индикатор загрузки, запускаю отрисовку приложения.
 document.addEventListener('DOMContentLoaded', async () => {
   const container = document.querySelector('.container');
   const loader = new PreloaderComponent('../assets/loading.svg')
@@ -26,38 +28,48 @@ document.addEventListener('DOMContentLoaded', async () => {
       loader.deleteFromDOM();
       renderApp(container);
     })
-    .catch(console.log);
+    .catch(e => {
+      console.log(e);
+      container.innerHTML = '<span class="message">Something is wrong :(</span>';
+    });
 });
 
 function renderApp(container) {
-  const form = container.querySelector('.form');
-  const userlist = container.querySelector('.userlist');
+  const form = container.querySelector('#form');
+  const userlist = container.querySelector('#userlist');
 
+  //Функция отрисовки списка пользователей
+  //(или сообщения о неудаче, если нет совпадений с текстом в фильтре).
   const renderUserList = (users) => {
     userlist.innerHTML = (users.length === 0) ?
-      '<span class="notfound">User not found</span>' :
+      '<span class="message">User not found</span>' :
       UserListComponent(users);
   };
 
+  //Отрисовка фильтра и списка пользоватлей.
   FilterComponent.addToDOM(form);
   renderUserList(state.users);
 
-
+  //Обработчик события ввода текста в поле Search.
+  //Отрисовывает пользователей если их имя соответствует введенному в поле значению.
   let onChange = (event) => {
     let searchText = event.target.value.toLowerCase();
     renderUserList(state.users.filter(user => user.name.toLowerCase().includes(searchText)));
   };
 
-  const debounce = (fn, wait) => {
+  //Отменяет повторную отрисовку списка пользователей в случае повторного
+  //ввода раньше чем через interval миллисекунд
+  const debounce = (fn, interval) => {
     let timer;
     return function () {
       clearTimeout(timer);
-      timer = setTimeout(() => fn.apply(this, arguments), wait);
+      timer = setTimeout(() => fn.apply(this, arguments), interval);
     }
   };
 
-  onChange = debounce(onChange, 500);
+  onChange = debounce(onChange, 300);
 
+  //Вешаю прослушку событий на форму и поле ввода
   const search = form.querySelector('input[type="search"]');
 
   search.addEventListener("input", onChange);
